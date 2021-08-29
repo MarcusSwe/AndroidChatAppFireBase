dd229aa6ea2c9ed3a2174a87ede3acaa49df4139
unknown <marcus.larsen.swe@gmail.com>
2021-08-29 15:52:57 +0200

    add user and read user password

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -11,11 +12,21 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageFocus;
     EditText editTextName;
     EditText editTextPassword;
+
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
+
+        database = FirebaseDatabase.getInstance();
+
+        DatabaseReference ref = database.getReference("users").child("testuser"); //add new users
+
+        ref.setValue("testlosen");
+
     }
 
     public void onClickImageView(View view) {
@@ -36,18 +54,42 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClickloginButton(View view) {
-        String getName = editTextName.getText().toString();
-        String getPassword = editTextPassword.getText().toString();
+      //  String getName = editTextName.getText().toString();
 
+        final String[] getName = {""};
         Intent goToChat = new Intent(this, chatRum.class);
-        Bundle extras = new Bundle();
 
-        Log.d("CPCP", getName);
+        DatabaseReference ref = database.getReference("users").child("testuser");
 
-        extras.putString("incoming_name", getName);
-        extras.putString("incoming_password", getPassword);
-        goToChat.putExtras(extras);
-        startActivity(goToChat);
+        ref.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    getName[0] = String.valueOf(task.getResult().getValue());
+
+                    String getPassword = editTextPassword.getText().toString();
+
+
+                    Bundle extras = new Bundle();
+
+                    Log.d("firebase", "sadgasdf");
+
+                    extras.putString("incoming_name", getName[0]);
+                    extras.putString("incoming_password", getPassword);
+                    goToChat.putExtras(extras);
+                    startActivity(goToChat);
+                }
+
+            }
+        });
+
+
+
+
 
 
     }
